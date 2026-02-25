d79a314fd72917d44f07a0cab4d94121899dfb71
Alex_kl <iissmirnov1995@gmail.com>
2026-02-25 23:35:11 +0300

    get_tree and tree_parser were added

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -2,6 +2,7 @@ import sys, os
 from zlib import decompress
 
 
+
 def get_branches(path):
     if not os.path.exists(path):
         raise ValueError("Error. Path does not exist")
@@ -34,6 +35,43 @@ def get_last_commit(path, branch):
     return commit.decode('utf-8')
 
 
+def tree_parser(tree):
+    tree = tree[tree.index(b'\x00') + 1:]
+    ind = 0
+    tree_list = []
+    while ind < len(tree):
+        mode = tree[ind:tree.index(b' ', ind)]
+        if mode == b'40000':
+            mode = 'tree'
+        else: 
+            mode = 'blob'
+
+        name = tree[tree.index(b' ', ind) + 1: tree.index(b'\x00', ind)]
+        name = name.decode('utf-8')
+        ind = tree.index(b'\x00', ind) + 1
+        obj_hash = tree[ind:ind + 20].hex()
+        ind += 20
+        tree_list.append(mode + ' ' + obj_hash + '    ' + name)
+    return tree_list
+    
+
+def get_tree(path, commit):
+    commit = commit.split('\n')
+    for line in commit:
+         line = line.split()
+         if line and line[0] == "tree":
+             tree_hash = line[1]
+             break
+         
+    path_tree = os.path.join(path, ".git", "objects", tree_hash[:2], tree_hash[2:])
+    with open(path_tree, "rb") as f:
+        tree = f.read()
+    
+    tree = decompress(tree)
+    tree = tree_parser(tree)
+    return tree
+
+    
 def main():
     argv = sys.argv
     if len(argv) == 1:
@@ -51,6 +89,9 @@ def main():
         try:
             commit_last = get_last_commit(argv[1], argv[2])
             print(commit_last)
+            tree = get_tree(argv[1], commit_last)
+            for i in tree:
+                print(i)
         except ValueError as error:
             print(error)
             exit(1)
