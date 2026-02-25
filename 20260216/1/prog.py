c23338fe25a1af910d9ade02fb53fb7a26e58651
Alex_kl <iissmirnov1995@gmail.com>
2026-02-25 22:37:48 +0300

    get_last_commit was added

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -1,4 +1,6 @@
 import sys, os
+from zlib import decompress
+
 
 def get_branches(path):
     if not os.path.exists(path):
@@ -10,9 +12,26 @@ def get_branches(path):
     path = os.path.join(path, ".git")
     if not os.path.isdir(path):
         raise ValueError("Error. Not a git repository")
+    
     path = os.path.join(path, "refs", "heads")
     items = os.listdir(path)
     return items
+
+
+def get_last_commit(path, branch):
+    if branch not in get_branches(path):
+        raise ValueError("Error. No such branch")
+
+    path_branch = os.path.join(path, ".git", "refs", "heads", branch)
+    with open(path_branch) as f:
+        commit_hash = f.read().strip()
+    
+    path_commit = os.path.join(path, ".git", "objects", commit_hash[:2], commit_hash[2:])
+    with open(path_commit, "rb") as f:
+        commit = f.read()
+    commit = decompress(commit)
+    commit = commit[commit.index(b'\x00') + 1:]
+    return commit.decode('utf-8')
 
 
 def main():
@@ -29,7 +48,13 @@ def main():
             print(error)
             exit(1)
     elif len(argv) == 3:
-        pass
+        try:
+            commit_last = get_last_commit(argv[1], argv[2])
+            print(commit_last)
+        except ValueError as error:
+            print(error)
+            exit(1)
+
     else:
         print("Error. Too many parameters were given")
         exit(1)
