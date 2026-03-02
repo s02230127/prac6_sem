01cdde497e2450677a88244df99d0771ed8c59d8
Alex_kl <iissmirnov1995@gmail.com>
2026-03-02 23:40:17 +0300

    Multi-user dungeon for task1

---
+++ 20260226/1/prog.py
@@ -0,0 +1,84 @@
+import sys
+import re
+import cowsay
+
+SIZE_X = 10
+SIZE_Y = 10
+START_X = 0
+START_Y = 0
+
+
+class Field:
+    def __init__(self, size_x=10, size_y=10, start_x=0, start_y=0):
+        self.player = (start_x % size_x, start_y % size_y)
+        self.monsters = {}
+        self.size_x = size_x
+        self.size_y = size_y
+
+
+def field_addmon(field, line):
+    if (matched := re.search(r'^addmon\s+(\d+)\s+(\d+)\s+(\S+)$', line)):
+        x = int(matched.group(1))
+        y = int(matched.group(2))
+
+        if not (0 <= x < field.size_x and 0 <= y < field.size_y):
+            raise ValueError
+        word = matched.group(3)
+        fl_replaced = (x, y) in field.monsters
+        field.monsters[(x, y)] = word
+        print(f"Added monster to ({x}, {y}) saying {word}")
+        if fl_replaced:
+            print("Replaced the old monster")
+    else:
+        raise ValueError
+
+
+def encounter(field, x, y):
+    print(cowsay.cowsay(field.monsters[(x, y)]))
+
+
+def player_moving(field, line):
+    if line == 'up':
+        field.player = (field.player[0], (field.player[1] - 1) % field.size_y)
+
+    elif line == 'down':
+        field.player = (field.player[0], (field.player[1] + 1) % field.size_y)
+
+    elif line == 'right':
+        field.player = ((field.player[0] + 1) % field.size_x, field.player[1])
+
+    elif line == 'left':
+        field.player = ((field.player[0] - 1) % field.size_x, field.player[1])
+
+    x, y = field.player
+    print(f"Moved to ({x}, {y})")
+
+    if (x, y) in field.monsters:
+        encounter(field, x, y)
+
+
+def command_reader(field):
+    for line in sys.stdin:
+        line = line.strip()
+
+        if not line:
+            continue
+
+        if line in ("up", "down", "left", "right"):
+            player_moving(field, line)
+        elif line.split()[0] == "addmon":
+            try:
+                field_addmon(field, line)
+            except ValueError:
+                print("Invalid arguments")
+        else:
+            print("Invalid command")
+
+
+def game():
+    field = Field(SIZE_X, SIZE_Y, START_X, START_Y)
+    command_reader(field)
+
+
+if __name__ == '__main__':
+    game()
