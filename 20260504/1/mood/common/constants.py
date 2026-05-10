0addb1bcd302d5848f82cc08e4b4c77c0a3dd81c
Alex_kl <iissmirnov1995@gmail.com>
2026-05-10 17:57:01 +0300

    Start of  HW

---
+++ 20260504/1/mood/common/constants.py
@@ -0,0 +1,13 @@
+"""Common constants for MOOD client and server."""
+
+HOST = "localhost"
+PORT = 7779
+
+SIZE_X = 10
+SIZE_Y = 10
+START_X = 0
+START_Y = 0
+
+INVALID_ARGS = 2
+
+SECONDS_FOR_WANDER = 30
